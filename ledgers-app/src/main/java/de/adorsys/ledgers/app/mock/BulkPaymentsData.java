ab711cbb2640de4d9ed11d045afe6526cd60d082
ESUM BRANDON <Esum.Brandon@adorsys.com>
2024-04-16 11:20:45 +0100

    Implimentation of Bulk paymentIT

--- ledgers-app/src/main/java/de/adorsys/ledgers/app/mock/BulkPaymentsData.java
+++ ledgers-app/src/main/java/de/adorsys/ledgers/app/mock/BulkPaymentsData.java
@@ -7,7 +7,9 @@ package de.adorsys.ledgers.app.mock;
 
 import de.adorsys.ledgers.middleware.api.domain.payment.PaymentTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class BulkPaymentsData extends BalancesData {
     private PaymentTO bulkPayment;
