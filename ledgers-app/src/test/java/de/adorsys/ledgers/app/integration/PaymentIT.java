fca1a4a2550123438811d04f40fea5220f7a12b2
ESUM BRANDON <Esum.Brandon@adorsys.com>
2024-04-16 13:16:01 +0100

    Bug fix on issues concerning "BULK" and others.

--- ledgers-app/src/test/java/de/adorsys/ledgers/app/integration/PaymentIT.java
+++ ledgers-app/src/test/java/de/adorsys/ledgers/app/integration/PaymentIT.java
@@ -129,8 +129,8 @@ public class PaymentIT extends BaseContainersTest<ManagementStage, OperationStag
                 .obtainTokenFromKeycloak(PSU_LOGIN, PSU_PASSWORD);
 
         when()
-                .failedSinglePayment("payment.json", "DE80760700240271232400");
-//                .failedBulkPayment("bulk_payment.json", "DE80760700240271232400");
+                .failedSinglePayment("payment.json", "DE80760700240271232400")
+                .failedBulkPayment("bulk_payment.json", "DE80760700240271232400");
 
         then()
                 .pathStr("devMessage", message -> assertThat(message).isEqualTo("Access Denied! You're trying to access resources you have no permission for."));
