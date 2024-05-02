4638d09de3e168a1a2f30c130dd46e0e8cb3de76
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2024-05-02 17:40:53 +0300

    fix pmd.

--- ledgers-app/src/test/java/de/adorsys/ledgers/app/integration/UserManagementIT.java
+++ ledgers-app/src/test/java/de/adorsys/ledgers/app/integration/UserManagementIT.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 
-import static de.adorsys.ledgers.app.Const.*;
+import static de.adorsys.ledgers.app.Const.*;//NOPMD
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ActiveProfiles({"testcontainers-it", "sandbox"})
