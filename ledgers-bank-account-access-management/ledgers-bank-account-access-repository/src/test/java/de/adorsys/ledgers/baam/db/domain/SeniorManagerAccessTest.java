0ddd319605f7965ab7dc8dd28b0ae0687216f7f1
Menkene Koufan <Menkene.Koufan@adorsys.com>
2024-09-27 12:11:20 +0100

    Created test for SeniorManagerAccess repository

---
+++ ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/test/java/de/adorsys/ledgers/baam/db/domain/SeniorManagerAccessTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2018-2023 adorsys GmbH and Co. KG
+ * All rights are reserved.
+ */
+
+package de.adorsys.ledgers.baam.db.domain;
+
+import org.junit.jupiter.api.Test;
+class SeniorManagerAccessTest {
+
+    @Test
+    void createManagerRole() {
+    }
+
+    @Test
+    void modifyManagerRole() {
+    }
+
+    @Test
+    void revokeManagerRole() {
+    }
+
+    @Test
+    void suspendAccess() {
+    }
+
+    @Test
+    void activateAccess() {
+    }
+}
