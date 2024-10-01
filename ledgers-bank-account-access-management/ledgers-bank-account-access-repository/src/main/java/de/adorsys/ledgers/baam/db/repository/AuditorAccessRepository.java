e0e6964ae440f759242751507b070c58f07dbe5f
Menkene Koufan <Menkene.Koufan@adorsys.com>
2024-10-01 16:33:50 +0100

    chore: created and tested auditor access entity

---
+++ ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/repository/AuditorAccessRepository.java
@@ -0,0 +1,10 @@
+package de.adorsys.ledgers.baam.db.repository;
+
+import de.adorsys.ledgers.baam.db.domain.AuditorAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditorAccessRepository extends JpaRepository<AuditorAccess, String> {
+    // Additional query methods can be added here if necessary
+}
