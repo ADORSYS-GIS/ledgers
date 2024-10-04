14176e8fba7cded4e28fb1d42b120b4a9d11afdd
yvanhenang <yvan.henang@skyengpro.com>
2024-10-04 12:37:03 +0100

    feat: updated ManagerAccess and TypeOfManagedAccess models

---
+++ ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/domain/ManagerAccess.java
@@ -0,0 +1,30 @@
+package de.adorsys.ledgers.baam.db.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ManagerAccess extends BankAccountAccess {
+
+    private String name;
+
+    private double weight; // Level of access (0 to 1)
+
+
+    @ElementCollection
+    private Set<TypeOfManagedAccess> managedAccessTypes = new HashSet<>();
+
+    @ElementCollection
+    private Set<AccessScope> scopeOfAccess = new HashSet<>();
+
+}
