6f474b3e98ec350964ec786fd1df1f6bca027e10
yvanhenang <yvan.henang@skyengpro.com>
2024-10-07 14:24:55 +0100

    Update TellerAccess

--- ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/domain/TellerAccess.java
+++ ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/domain/TellerAccess.java
@@ -1,40 +1,16 @@
 package de.adorsys.ledgers.baam.db.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import jakarta.persistence.*;
-import java.util.Date;
-import java.util.UUID;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class TellerAccess  {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-
-    private String name;
-    private String tellerId;
-
-    @Enumerated(EnumType.STRING)
-    private AccessStatus status;
+public class TellerAccess extends BankAccountAccess  {
 
     private double dailyLimit;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private String accessType = "TELLER_ACCESS";
 }
--- ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/repository/TellerAccessRepository.java
+++ ledgers-bank-account-access-management/ledgers-bank-account-access-repository/src/main/java/de/adorsys/ledgers/baam/db/repository/TellerAccessRepository.java
@@ -5,9 +5,7 @@ import de.adorsys.ledgers.baam.db.domain.TellerAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface TellerAccessRepository extends JpaRepository<TellerAccess, UUID> {
+public interface TellerAccessRepository extends JpaRepository<TellerAccess, String> {
 
 }
