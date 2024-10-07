69b54f83939e4faabd9b82414b7ce5a802c58613
Elwizzy12 <chiahans123@gmail.com>
2024-10-07 16:43:54 +0100

    modify exceptions mapped request

--- ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositHttpStatusResolver.java
+++ ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositHttpStatusResolver.java
@@ -25,20 +25,34 @@ public class DepositHttpStatusResolver {
         container.put(PAYMENT_NOT_FOUND, NOT_FOUND);
 
         //400 Block
-        container.put(INSUFFICIENT_FUNDS, BAD_REQUEST);
+        // This error is thrown when attempting to create a deposit account that already exists.
         container.put(DEPOSIT_ACCOUNT_EXISTS, BAD_REQUEST);
-        container.put(PAYMENT_PROCESSING_FAILURE, BAD_REQUEST);
+
+        // This error is thrown when a failure occurs during the processing of a payment, typically due to server-side issues.
+        container.put(PAYMENT_PROCESSING_FAILURE, INTERNAL_SERVER_ERROR);
+
+        // This error is thrown when an attempt is made to process a payment with an ID that already exists in the system.
         container.put(PAYMENT_WITH_ID_EXISTS, BAD_REQUEST);
+
+        // This error is thrown when there is a failure in performing a deposit operation, such as adding or removing funds from an account.
         container.put(DEPOSIT_OPERATION_FAILURE, BAD_REQUEST);
+
+        // This error is thrown when a currency that is not supported by the system is used in a transaction.
         container.put(CURRENCY_NOT_SUPPORTED, BAD_REQUEST);
+
+        // This error is thrown when an unsupported credit limit is set, either exceeding the allowed limit or being invalid for the account.
         container.put(UNSUPPORTED_CREDIT_LIMIT, BAD_REQUEST);
 
+        //402 Block
+        // This error is thrown when there are insufficient funds in the deposit account to complete the requested transaction.
+        container.put(INSUFFICIENT_FUNDS, PAYMENT_REQUIRED);
+
         //417 Block
+
         container.put(COULD_NOT_EXECUTE_STATEMENT, EXPECTATION_FAILED);
     }
-
     public static HttpStatus resolveHttpStatusByCode(DepositErrorCode code) {
-        return container.get(code);
+        return container.getOrDefault(code, BAD_REQUEST) ;
     }
 }
 
