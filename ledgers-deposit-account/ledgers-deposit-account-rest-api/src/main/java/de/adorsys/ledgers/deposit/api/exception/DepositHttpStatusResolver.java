ebc81fb3f6b5cc167961df6a69b3c9c2b8bc4a9b
Elwizzy12 <chiahans123@gmail.com>
2024-10-08 08:05:25 +0100

    update exception error mappings

--- ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositHttpStatusResolver.java
+++ ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/exception/DepositHttpStatusResolver.java
@@ -25,14 +25,6 @@ public class DepositHttpStatusResolver {
         container.put(PAYMENT_NOT_FOUND, NOT_FOUND);
 
         //400 Block
-        // This error is thrown when attempting to create a deposit account that already exists.
-        container.put(DEPOSIT_ACCOUNT_EXISTS, BAD_REQUEST);
-
-        // This error is thrown when a failure occurs during the processing of a payment, typically due to server-side issues.
-        container.put(PAYMENT_PROCESSING_FAILURE, INTERNAL_SERVER_ERROR);
-
-        // This error is thrown when an attempt is made to process a payment with an ID that already exists in the system.
-        container.put(PAYMENT_WITH_ID_EXISTS, BAD_REQUEST);
 
         // This error is thrown when there is a failure in performing a deposit operation, such as adding or removing funds from an account.
         container.put(DEPOSIT_OPERATION_FAILURE, BAD_REQUEST);
@@ -43,13 +35,23 @@ public class DepositHttpStatusResolver {
         // This error is thrown when an unsupported credit limit is set, either exceeding the allowed limit or being invalid for the account.
         container.put(UNSUPPORTED_CREDIT_LIMIT, BAD_REQUEST);
 
-        //402 Block
+        //403 Block
         // This error is thrown when there are insufficient funds in the deposit account to complete the requested transaction.
-        container.put(INSUFFICIENT_FUNDS, PAYMENT_REQUIRED);
+        container.put(INSUFFICIENT_FUNDS, FORBIDDEN);
 
         //417 Block
 
         container.put(COULD_NOT_EXECUTE_STATEMENT, EXPECTATION_FAILED);
+
+        //500
+        // This error is thrown when a failure occurs during the processing of a payment, typically due to server-side issues.
+        container.put(PAYMENT_PROCESSING_FAILURE, INTERNAL_SERVER_ERROR);
+
+        //409
+        // This error is thrown when an attempt is made to process a payment with an ID that already exists in the system.
+        container.put(PAYMENT_WITH_ID_EXISTS, CONFLICT);
+        // This error is thrown when attempting to create a deposit account that already exists.
+        container.put(DEPOSIT_ACCOUNT_EXISTS, CONFLICT);
     }
     public static HttpStatus resolveHttpStatusByCode(DepositErrorCode code) {
         return container.getOrDefault(code, BAD_REQUEST) ;
