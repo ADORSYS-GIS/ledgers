2af0fe381da389d974f1dd218bb9ced92daa46e1
Francis Pouatcha <francis.pouatcha@adorsys.com>
2024-10-29 08:48:38 +0100

    Trimming the resp api to required endpoints

--- ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/resource/AccountRestAPI.java
+++ ledgers-deposit-account/ledgers-deposit-account-rest-api/src/main/java/de/adorsys/ledgers/deposit/api/resource/AccountRestAPI.java
@@ -32,35 +32,30 @@ import static de.adorsys.ledgers.deposit.api.utils.Constants.SIZE;
 import static de.adorsys.ledgers.deposit.api.utils.Constants.TRANSACTION_ID;
 
 
-@Tag(name = "LDG003 - Accounts", description = "Provides access to a deposit account. This interface does not provide any endpoint to list all accounts.")
+@Tag(name = "LDG??? - Accounts", description = "Provides access to a deposit account.")
 public interface AccountRestAPI {
     String BASE_PATH = "/accounts";
 
-
     @GetMapping("/{accountId}")
     @Operation(summary = "Load Account by AccountId",
-            description = "Returns account details information for the given account id. "
-                    + "User must have access to the target account. This is also accessible to other token types like tpp token (DELEGATED_ACESS)")
+            description = "Returns account details information for the given account id. ")
   
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = AccountDetailsTO.class)),
                     description = "Account details.")
     })
     ResponseEntity<AccountDetailsTO> getAccountDetailsById(@Parameter(name = ACCOUNT_ID) @PathVariable(ACCOUNT_ID) String accountId);
+
     @GetMapping("/{accountId}/balances")
     @Operation(summary = "Read balances",
-            description = "Returns balances of the deposit account with the given accountId. "
-                    + "User must have access to the target account. This is also accessible to other token types like tpp token (DELEGATED_ACESS)")
-
+            description = "Returns balances of the deposit account with the given accountId.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = AccountBalanceTO.class)), description = "List of accounts balances for the given account.")
     })
-
     ResponseEntity<List<AccountBalanceTO>> getBalances(@Parameter(name = ACCOUNT_ID) @PathVariable(ACCOUNT_ID) String accountId);
 
     @GetMapping(path = "/{accountId}/transactions", params = {DATE_FROM_QUERY_PARAM, DATE_TO_QUERY_PARAM})
     @Operation(summary = "Find Transactions By Date", description = "Returns all transactions for the given account id")
-
     ResponseEntity<List<TransactionTO>> getTransactionByDates(
             @Parameter(name = ACCOUNT_ID)
             @PathVariable(ACCOUNT_ID) String accountId,
@@ -69,7 +64,6 @@ public interface AccountRestAPI {
 
     @GetMapping(path = "/{accountId}/transactions/page", params = {DATE_FROM_QUERY_PARAM, DATE_TO_QUERY_PARAM, PAGE, SIZE})
     @Operation(summary = "Find Transactions By Date", description = "Returns transactions for the given account id for certain dates, paged view")
-
     ResponseEntity<CustomPageImpl<TransactionTO>> getTransactionByDatesPaged(
             @Parameter(name = ACCOUNT_ID)
             @PathVariable(name = ACCOUNT_ID) String accountId,
@@ -80,9 +74,10 @@ public interface AccountRestAPI {
 
     @GetMapping("/{accountId}/transactions/{transactionId}")
     @Operation(summary = "Load Transaction", description = "Returns the transaction with the given account id and transaction id.")
-
     ResponseEntity<TransactionTO> getTransactionById(
             @Parameter(name = ACCOUNT_ID)
             @PathVariable(name = ACCOUNT_ID) String accountId,
             @Parameter(name = TRANSACTION_ID)
-            @PathVariable(name = TRANSACTION_ID) String transactionId);}
+            @PathVariable(name = TRANSACTION_ID) String transactionId);
+        
+}
--- ledgers-deposit-account/ledgers-deposit-account-service-api/src/main/java/de/adorsys/ledgers/deposit/api/domain/account/TransactionTO.java
+++ ledgers-deposit-account/ledgers-deposit-account-service-api/src/main/java/de/adorsys/ledgers/deposit/api/domain/account/TransactionTO.java
@@ -22,33 +22,58 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionTO {
+
     private String transactionId;
+    
     private String entryReference;
+    
     private String endToEndId;
+    
     private String mandateId;
+    
     private String checkId;
+    
     private String creditorId;
+    
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate bookingDate;
+    
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate valueDate;
+    
     private AmountTO amount;
+
     private List<ExchangeRateTO> exchangeRate;
+    
     private String creditorName;
+    
     private AccountReferenceTO creditorAccount;
+    
     private String creditorAgent;
+    
     private String ultimateCreditor;
+    
     private String debtorName;
+    
     private AccountReferenceTO debtorAccount;
+    
     private String debtorAgent;
+    
     private String ultimateDebtor;
+    
     private String additionalInformation;
+    
     private List<String> remittanceInformationUnstructuredArray;
+    
     private List<RemittanceInformationStructuredTO> remittanceInformationStructuredArray;
+    
     private String purposeCode;
+    
     private String bankTransactionCode;
+    
     private String proprietaryBankTransactionCode;
+    
     private AccountBalanceTO balanceAfterTransaction;
 }
